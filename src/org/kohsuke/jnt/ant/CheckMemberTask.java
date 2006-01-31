144c8aca86bc856c1aebb1c44679abb949eb7428
Kohsuke Kawaguchi <kk@kohsuke.org>
2006-01-31 15:45:55 +0000

    implemented the check member task.

---
+++ src/org/kohsuke/jnt/ant/CheckMemberTask.java
@@ -0,0 +1,47 @@
+package org.kohsuke.jnt.ant;
+
+import org.kohsuke.jnt.JNProject;
+import org.kohsuke.jnt.ProcessingException;
+import org.kohsuke.jnt.JavaNet;
+import org.kohsuke.jnt.JNRole;
+import org.apache.tools.ant.BuildException;
+
+import java.util.Set;
+
+/**
+ * Checks if an user belongs to a project.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class CheckMemberTask extends AbstractJavaNetTaskForProject {
+    private String role;
+    private String user;
+    private String property;
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+    protected void run(JNProject project) throws ProcessingException, BuildException {
+        JavaNet conn = project.getConnection();
+        Set<JNRole> roles = project.getMembership().getRolesOf(conn.getUser(user));
+
+        boolean r;
+        if(role!=null)
+            r = roles.contains(role);
+        else
+            r = !roles.isEmpty();
+
+        if(r) {
+            getProject().setProperty(property, "true" );
+        }
+    }
+}
