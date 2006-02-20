8be5f837937e5b4b9d83ede0b5d7fe7b68054adc
Kohsuke Kawaguchi <kk@kohsuke.org>
2006-02-20 17:39:22 +0000

    we now have 6 mailing lists on jaxb.

--- test/org/kohsuke/jnt/JNMailingListsTest.java
+++ test/org/kohsuke/jnt/JNMailingListsTest.java
@@ -17,7 +17,7 @@ public class JNMailingListsTest extends TestCaseBase {
         JNProject project = con.getProject("jaxb");
         JNMailingLists lists = project.getMailingLists();
 
-        assertEquals(5,lists.getLists().size());
+        assertEquals(6,lists.getLists().size());
 
         JNMailingList list1 = lists.getLists().get(0);
         assertEquals("dev",list1.getName());
